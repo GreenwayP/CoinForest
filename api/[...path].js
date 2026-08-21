55e9702904e34c5045a282175010734c0a4d4312
GreenwayP <michgeorge0452@gmail.com>
2026-08-21 03:18:29 +0100

    Implement 404 handler for API route

--- api/[...path].js
+++ api/[...path].js
@@ -1,1 +1,14 @@
-export { default } from "./index.js";
+export default function handler(request) {
+  return new Response(
+    JSON.stringify({
+      success: false,
+      error: "API route not found."
+    }),
+    {
+      status: 404,
+      headers: {
+        "Content-Type": "application/json; charset=utf-8"
+      }
+    }
+  );
+}
