52d1ff31e3e6971e63a356799a02f9cf9650ec52
GreenwayP <michgeorge0452@gmail.com>
2026-08-21 03:21:45 +0100

    Refactor API handler to export from index.js

--- api/[...path].js
+++ api/[...path].js
@@ -1,14 +1,1 @@
-export default function handler(request) {
-  return new Response(
-    JSON.stringify({
-      success: false,
-      error: "API route not found."
-    }),
-    {
-      status: 404,
-      headers: {
-        "Content-Type": "application/json; charset=utf-8"
-      }
-    }
-  );
-}
+export { default } from "./index.js";
